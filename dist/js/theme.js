a6749badbc866f1b2f88488ed4e09d666f0bab23
Hooniya-nolja <wkdckdgns1017@gmail.com>
2021-09-09 15:46:20 +0900

    docs: (mobile) navbar 화살표에 changeArrow 함수 추가

--- dist/js/theme.js
+++ dist/js/theme.js
@@ -1048,3 +1048,14 @@ function _typeof(obj) { "@babel/helpers - typeof"; if (typeof Symbol === "functi
     }
   }();
 })();
+
+
+function changeArrow() {
+  let navbarServiceRow = document.getElementById('navbarServiceRow');
+  let arrow = document.getElementById('arrow');
+  if (navbarServiceRow.className.indexOf('show') < 0) {   // 판단 시점은 클릭하기 전 단계.
+    arrow.className = "ai-chevron-up d-lg-none ms-1 mb-1";
+  } else {
+    arrow.className = "ai-chevron-down d-lg-none ms-1 mb-1";
+  }
+}

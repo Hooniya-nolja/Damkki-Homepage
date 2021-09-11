492d065fba123a17b11bacff268f48e01ca6a7b5
Hooniya-nolja <wkdckdgns1017@gmail.com>
2021-09-11 21:19:39 +0900

    docs: 가맹음식점 안내에 toast 추가, 박스그림자 생성

--- dist/js/theme-damkki.js
+++ dist/js/theme-damkki.js
@@ -8,3 +8,27 @@ function changeArrow() {
       arrow.className = "ai-chevron-down d-lg-none ms-1 mb-1";
     }
 }
+
+
+// KakaoMap API
+let container = document.getElementById('kakaomap');
+let options = {
+  center: new kakao.maps.LatLng(37.41454903496425, 127.13462691306036),
+  level: 3
+};
+
+let map = new kakao.maps.Map(container, options);
+
+let imageSrc = "/img/damggi/service_area/map_marker.svg",
+    imageSize = new kakao.maps.Size(40,55),
+    imageOption = {offset: new kakao.maps.Point(20, 54)};
+
+let markerImage = new kakao.maps.MarkerImage(imageSrc, imageSize, imageOption),
+    markerPosition = new kakao.maps.LatLng(37.41454903496425, 127.13462691306036);
+
+let marker = new kakao.maps.Marker({
+    position: markerPosition, 
+    image: markerImage
+});
+
+marker.setMap(map);

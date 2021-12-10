38f0c9a63c835ff36a773edb4f975453b9a57248
Hooniya-nolja <wkdckdgns1017@gmail.com>
2021-12-10 21:08:50 +0900

    docs: 이미지들 수정, 페이지하단 그리노늘 정보 추가, 서비스지역 구조수정, 메일 기능 추가

--- dist/js/theme-damkki.js
+++ dist/js/theme-damkki.js
@@ -11,24 +11,24 @@ function changeArrow() {
 
 
 // KakaoMap API
-let container = document.getElementById('kakaomap');
-let options = {
-  center: new kakao.maps.LatLng(37.41454903496425, 127.13462691306036),
-  level: 3
-};
+// let container = document.getElementById('kakaomap');
+// let options = {
+//   center: new kakao.maps.LatLng(37.41454903496425, 127.13462691306036),
+//   level: 3
+// };
 
-let map = new kakao.maps.Map(container, options);
+// let map = new kakao.maps.Map(container, options);
 
-let imageSrc = "/img/damggi/service_area/map_marker.svg",
-    imageSize = new kakao.maps.Size(40,55),
-    imageOption = {offset: new kakao.maps.Point(20, 54)};
+// let imageSrc = "/img/damggi/service_area/map_marker.svg",
+//     imageSize = new kakao.maps.Size(40,55),
+//     imageOption = {offset: new kakao.maps.Point(20, 54)};
 
-let markerImage = new kakao.maps.MarkerImage(imageSrc, imageSize, imageOption),
-    markerPosition = new kakao.maps.LatLng(37.41454903496425, 127.13462691306036);
+// let markerImage = new kakao.maps.MarkerImage(imageSrc, imageSize, imageOption),
+//     markerPosition = new kakao.maps.LatLng(37.41454903496425, 127.13462691306036);
 
-let marker = new kakao.maps.Marker({
-    position: markerPosition, 
-    image: markerImage
-});
+// let marker = new kakao.maps.Marker({
+//     position: markerPosition, 
+//     image: markerImage
+// });
 
-marker.setMap(map);
+// marker.setMap(map);
